c64056b51f14f5d73af50885c981e102e60eda6e
Alex MacLean <alex.maclean2000@gmail.com>
2022-02-06 17:50:42 -0800

    cleanup

--- scrach-work/nvidia-nemo-experimenting/main.py
+++ scrach-work/nvidia-nemo-experimenting/main.py
@@ -1,10 +1,27 @@
-print('a')
-
 import nemo.collections.asr as nemo_asr
 import numpy as np
 import librosa
 
+AUDIO_FILENAME = "./test-audio-2.wav" # the unprocessed audio breaks the model
 
-print('a')
+def softmax(logits):
+    e = np.exp(logits - np.max(logits))
+    return e / e.sum(axis=-1).reshape([logits.shape[0], 1])
+
+
 asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name='QuartzNet15x5Base-En', strict=False)
-print(asr_model)
+
+files = [AUDIO_FILENAME]
+transcript = asr_model.transcribe(paths2audio_files=files)[0]
+print(f'Transcript: "{transcript}"')
+
+
+logits = asr_model.transcribe(files, logprobs=True)[0]
+probs = softmax(logits)
+print(probs)
+
+# 20ms is duration of a timestep at output of the model
+time_stride = 0.02
+
+
+labels = list(asr_model.decoder.vocabulary) + ['blank']
