e0ae58dd9300ff72a9d3649db176c1a3b28cb5d1
Alex MacLean <alex.maclean2000@gmail.com>
2022-02-18 09:19:10 -0800

    basic model

--- project/lsp_model.py
+++ project/lsp_model.py
@@ -1,14 +1,29 @@
 
-from sympy import im
+import os
+from pathlib import PurePath
 
 
 from lsp_client import LspClient, LspConnection
 
+PYTHON_LSP = ['python', '-mpylsp']
+PYTHON_LANG_ID = 'python'
+
+
 class LspModel:
     def __init__(self) -> None:
-        lspc = LspClient(LspConnection(RACKET_LSP), path)
-        lspc.initialize()
-        doc_id = lspc.did_open('test.rkt', 'racket', '#lang racket')
-        lspc.completion(doc_id, 1, 2)
-        lspc.shutdown()
-        lspc.exit()
+        path = PurePath(os.getcwd())
+        self.lang_client = LspClient(LspConnection(PYTHON_LSP), path)
+        self.lang_client.initialize()
+        self.doc_id = self.lang_client.did_open('buffer', PYTHON_LANG_ID, '')
+
+
+    def get_completions(self, text) -> list[str]:
+        self.lang_client.did_change(self.doc_id, text)
+        response = self.lang_client.completion(self.doc_id, 0, len(text))
+        results = [r['insertText'] for r in response['result']['items']]
+        return results
+
+
+    def quit(self) -> None:
+        self.lang_client.shutdown()
+        self.lang_client.exit()
