70aa0397878bf50373e81709cc975ab1602505b3
Alex Maclean (WSL) <alex.maclean2000@gmail.com>
2022-02-21 15:25:25 -0800

    very slow progress...

--- project/asr.py
+++ project/asr.py
@@ -1,7 +1,22 @@
+import re
 import nemo.collections.asr as nemo_asr
 import numpy as np
 import librosa
 import os
+# from ctc_decoders import Scorer
+
+
+# class SneakyCustomScorer(Scorer):
+#     def __init__(self, scorer) -> None:
+#         pass
+
+    # def get_log_cond_prob(self, arg):
+    #     print(arg)
+    #     return self.scorer.get_log_cond_prob(arg)
+
+    # def get_sent_log_prob(self, arg):
+    #     print(arg)
+    #     return self.scorer.get_sent_log_prob(arg)
 
 
 def softmax(logits):
@@ -10,32 +25,55 @@ def softmax(logits):
 
 
 def basic_transcribe(audio_file):
+    print('')
+    print("01@@@@@@@@@@@@@@@@")
+    print('')
     asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name='QuartzNet15x5Base-En', strict=False)
 
     files = [audio_file]
     logits = asr_model.transcribe(files, logprobs=True)[0]
     probs = softmax(logits)
+    print('')
+    print("2@@@@@@@@@@@@@@@@")
+    print('')
     print(probs)
+
+    print('')
+    print("3@@@@@@@@@@@@@@@@")
+    print('')
 
     beam_search_lm = nemo_asr.modules.BeamSearchDecoderWithLM(
         vocab=list(asr_model.decoder.vocabulary),
         beam_width=16,
         alpha=2, beta=1.5,
-        lm_path='',
-        num_cpus=max(os.cpu_count(), 1),
+        lm_path='lowercase_3-gram.pruned.1e-7.arpa',
+        num_cpus=1,#max(os.cpu_count(), 1),
         input_tensor=False)
 
-    beam_search_lm.forward(log_probs=np.expand_dims(probs, axis=0), log_probs_length=None)
+
+    print('')
+    print("4@@@@@@@@@@@@@@@@")
+    print('')
+    # new_scorer = SneakyCustomScorer(beam_search_lm.scorer)
+
+    #beam_search_lm.scorer.__class__ = SneakyCustomScorer
+
+    print('')
+    print("5@@@@@@@@@@@@@@@@")
+    print('')
+
+    o = beam_search_lm.forward(log_probs=np.expand_dims(probs, axis=0), log_probs_length=None)
+    print('')
+    print("@@@@@@@@@@@@@@@@")
+    print('')
+
+    print(o)
 
 
-# 20ms is duration of a timestep at output of the model
-# time_stride = 0.02
-
-
-# labels = list(asr_model.decoder.vocabulary) + ['blank']
 
 if __name__ == "__main__":
     import argparse
+    print("00")
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help='Path to audio file')
